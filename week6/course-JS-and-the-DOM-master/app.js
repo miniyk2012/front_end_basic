c6c389c41f94d2c1b04e4a7fd24c215df7cbd998
Thomas Young <yk_ecust_2007@163.com>
2019-09-06 08:01:16 +0800

    [feature] remove, 改变属性

--- week6/course-JS-and-the-DOM-master/app.js
+++ week6/course-JS-and-the-DOM-master/app.js
@@ -7,4 +7,30 @@ let h1 = document.querySelector('h1');
 newSpan2 = document.createElement('span');
 newSpan2.textContent = '牛逼哄哄2';
 // h1.appendChild(newSpan);
-h1.append(newSpan,newSpan2, 'SB');
+h1.append(newSpan, newSpan2, 'SB');
+
+
+async function demo() {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    await sleep(1500);
+    // 法1
+    // let hero_module = document.querySelector('.hero__module');
+    // let firstCard = hero_module.firstElementChild;
+    // hero_module.removeChild(firstCard);
+
+    // 法2
+    // let firstCard = document.querySelector('.hero__module--container');
+    // firstCard.parentElement.removeChild(firstCard);
+
+    // 法3
+    let firstCard = document.querySelector('.hero__module--container');
+    firstCard.remove();
+    let header = document.querySelector('h6.text-center');
+    header.style.fontSize = '2em';
+    header.style.color = 'orange';
+    header.style.width = '50%';
+    header.style.cssText = 'color: blue; background-color: orange; font-size: 3.5em';
+}
+demo();
