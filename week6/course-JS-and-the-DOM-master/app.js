edbb58f62136e0826e20237ee56036f6e5114f09
Thomas Young <yk_ecust_2007@163.com>
2019-12-07 22:35:30 +0800

    sth

--- week6/course-JS-and-the-DOM-master/app.js
+++ week6/course-JS-and-the-DOM-master/app.js
@@ -1,14 +1,21 @@
 const nanodegreeCard = document.querySelector('.card');
 console.dir(nanodegreeCard);
+console.log(nanodegreeCard.textContent);
+nanodegreeCard.textContent = "dfa";
 
 let newSpan = document.createElement('span');
-newSpan.textContent = '牛逼哄哄1';
+newSpan.textContent = '<div>牛逼哄哄1</div>';
 let h1 = document.querySelector('h1');
 newSpan2 = document.createElement('span');
-newSpan2.textContent = '牛逼哄哄2';
+newSpan2.innerHTML = '<div>牛逼哄哄2</div>';
 // h1.appendChild(newSpan);
-h1.append(newSpan, newSpan2, 'SB');
+h1.append(newSpan, newSpan2, '<div>SB</div>');
 
+
+const innerResults = document.querySelector('#pick-me').innerHTML;
+const outerResults = document.querySelector('#pick-me').outerHTML;
+console.log(innerResults); // logs the string: "Greetings To <span>All</span>!"
+console.log(outerResults);  // los the string "<h1 id="pick-me">Greetings To <span>All</span>!</h1>"
 
 async function demo() {
     function sleep(ms) {
@@ -25,7 +32,7 @@ async function demo() {
     // firstCard.parentElement.removeChild(firstCard);
 
     // 法3
-    let firstCard = document.querySelector('.hero__module--container');
+    let firstCard = document.querySelector('.hero__module--container:nth-child(3)');
     firstCard.remove();
     let header = document.querySelector('h6.text-center');
     header.style.fontSize = '2em';
